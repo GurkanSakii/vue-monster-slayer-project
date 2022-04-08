a4348c9a8c7ab33bfaa757104fbedb5dd56e650d
SakiGurkan <83540692+GurkanSakii@users.noreply.github.com>
2022-04-08 11:10:08 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <MonsterSl />
+</template>
+
+<script>
+import MonsterSl from './components/MonsterSl.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MonsterSl
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
